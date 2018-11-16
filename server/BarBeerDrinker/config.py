fb6f97d5e1918eb068bff64d5850fc63a9d4ab18
gmoney1036 <gabrielrodriguez1036@gmail.com>
2018-11-15 21:25:28 -0500

    config fixed uri

--- server/BarBeerDrinker/config.py
+++ server/BarBeerDrinker/config.py
@@ -1,1 +1,1 @@
-database_uri = "mysql+pymysql://root:scarface@localhost:3306/bbdplus"
+database_uri = "mysql+pymysql://gsrtrj1213:Scarface1!@vegasbbdplustinagabe.cv0vjjyclwyv.us-east-1.rds.amazonaws.com:3306/gsrtrj1213"
